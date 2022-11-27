fe06f5a438de4b3a235ec826a51656cfd1f5041e
BubbleNeumann <bubbleneumann@gmail.com>
2022-11-27 11:45:02 +0400

    astronvim

---
+++ ubuntu-init.sh
@@ -0,0 +1,12 @@
+# sudo apt update -y && sudo apt upgrade -y;
+
+# install oh-my-zsh
+# TODO test this
+sh -c $(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh);
+# zsh config
+# 
+
+# astronvim
+# kitty
+# kitty config
+# 
