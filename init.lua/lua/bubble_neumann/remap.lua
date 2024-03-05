0539f690502a79d7589a4a70325b47b768269487
BubbleNeumann <53640776+BubbleNeumann@users.noreply.github.com>
2024-03-06 00:06:35 +0400

    keybindings for harpoon

--- init.lua/lua/bubble_neumann/remap.lua
+++ init.lua/lua/bubble_neumann/remap.lua
@@ -11,3 +11,11 @@ km("n", "<leader>c", '<cmd>q<cr>')
 km("v", "J", ":m '>+1<CR>gv=gv")
 km("v", "K", ":m '<-2<CR>gv=gv")
 
+-- harpoon
+-- https://github.com/ThePrimeagen/.dotfiles/blob/master/nvim/.config/nvim/after/plugin/keymap/harpoon.lua
+km('n', '<C-a>', '<cmd>lua require("harpoon.mark").add_file()<cr>', opts)
+km('n', '<leader>h', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>', opts)
+km('n', '<leader>a', '<cmd>lua require("harpoon.ui").nav_file(1)<cr>', opts)
+km('n', '<leader>s', '<cmd>lua require("harpoon.ui").nav_file(2)<cr>', opts)
+km('n', '<leader>d', '<cmd>lua require("harpoon.ui").nav_file(3)<cr>', opts)
+km('n', '<leader>f', '<cmd>lua require("harpoon.ui").nav_file(4)<cr>', opts)
